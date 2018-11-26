4e4142bcc0a7afa0868b2b516d76c384f6c72f8f
david <thyun.ahn@gmail.com>
2018-11-26 16:02:49 +0900

    add animation for slide up down menu in mobile

--- blogDesign/index.js
+++ blogDesign/index.js
@@ -1,6 +1,16 @@
 var menu_btn = document.querySelector(".button__toggle");
 
 menu_btn.addEventListener("click", (event) => {
-  var menu_item = document.querySelector(".navigation");
-  menu_item.classList.toggle("menu__item__hide");
+  var menu_item = document.getElementsByClassName("navigation")[0];
+  if (menu_item.getAttribute("class") == "navigation menu__item__hide") {
+    // menu_item.style.height = menu_item.scrollHeight + "px";
+    menu_item.setAttribute("class", "navigation menu__item__show");
+  } else {
+    menu_item.setAttribute("class", "navigation menu__item__hide");
+  }
+
+  // var result = menu_item.classList.toggle("menu__item__hide"); // false : menu show, true : menu hide
+  // console.log(result); //ERASE!!
+  // if (result == true) menu_item.style.height = "0px";
+  // else menu_item.style.height = menu_item.scrollHeight + "px"; //for show/hide animation.
 });
