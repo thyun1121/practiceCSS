252f8fafc22524668d04f2ba88fed0085e341251
david <thyun.ahn@gmail.com>
2018-12-06 01:02:48 +0900

    css coding for about page

---
+++ blogDesign/about.js
@@ -0,0 +1,36 @@
+var isMobile = {
+  Android: function() {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function() {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function() {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function() {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function() {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function() {
+    return (
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.iOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows()
+    );
+  }
+};
+
+var setHeightElement = document.getElementsByClassName("js__fullHeight")[0];
+
+var fullHeight = function() {
+  if (!isMobile.any()) {
+    setHeightElement.style.height = window.innerHeight + "px";
+  }
+};
+
+fullHeight();
